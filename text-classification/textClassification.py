7b59226c5ec9a4e77e94ed047854b1b37bbb11d1
chetan22 <ck.chetan20@gmail.com>
2019-04-27 15:24:35 +0530

    Add Vectorization to code This commit vectorizes the data to get a vocabulary dict. This dict is then used as features for logistic regression classifier.

--- text-classification/textClassification.py
+++ text-classification/textClassification.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
 
 # importing data
 # setting filepaths
@@ -12,6 +15,30 @@ for source, filepath in filepath_dict.items():
     df = pd.read_csv(filepath, names=['sentence', 'label'], sep='\t')
     df['source'] = source  # Add another column filled with the source name
     df_list.append(df)
-# un-comment to preview data files list
+
 df = pd.concat(df_list)
-print(df)
+# un-comment to preview data files list
+# print(df)
+
+# sepertating yelp data-set
+df_yelp = df[df['source'] == 'yelp']
+sentences = df_yelp['sentence'].values
+y = df_yelp['label'].values
+# spliiting into test-train data
+sentences_train, sentences_test, y_train, y_test = train_test_split(
+    sentences, y, test_size=0.25, random_state=1000)
+# vectorize using BOW model
+# create a countVectorizer instance for vectorizing
+vectorizer = CountVectorizer()
+# learning features by creating a vocabulary of all words in all sentences
+vectorizer.fit(sentences_train)
+# transforming learnt features into an array format
+X_train = vectorizer.transform(sentences_train)
+X_test = vectorizer.transform(sentences_test)
+# Applying logistic regression
+classifier = LogisticRegression()
+# fitting training data
+classifier.fit(X_train, y_train)
+# testing classifier accuracy on test data
+accuracy = classifier.score(X_test, y_test)
+print(accuracy)
