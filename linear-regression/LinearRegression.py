773ce3665ff578d6833b2b32f8d4bbd108697489
chetan22 <ck.chetan20@gmail.com>
2019-04-27 11:16:17 +0530

    Add linear regression

---
+++ linear-regression/LinearRegression.py
@@ -0,0 +1,37 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+# Loading training data
+fname = './single-feature.txt'
+X, y = np.loadtxt(fname, delimiter=',', unpack=True)
+X = X.reshape(-1, 1)
+y = y.reshape(-1, 1)
+# creating model
+model = LinearRegression()
+# Fitting training data
+model.fit(X, y)
+# Printing theta0 and theta1
+# intercept_ --> theta0 and coef_ --> list of [theta1, theta2, ...]
+print(model.intercept_, model.coef_)
+# Coeff of determination for training data
+print(model.score(X, y))
+# predicting for x = 3.5 and x = 7.0
+print(model.predict(np.array([3.5, 7.0]).reshape(-1, 1)))
+
+# Multi-feature training set
+fname = './multi-feature.txt'
+X1, X2, y = np.loadtxt(fname, delimiter=',', unpack=True)
+# X = np.hstack((X1, X2))
+X = np.c_[X1, X2]
+X = X.reshape(-1, 2)
+y = y.reshape(-1, 1)
+model.normalize = True
+model.fit(X, y)
+print(model.intercept_, model.coef_, model.score(X, y))
+print(model.predict(np.array([1650, 3]).reshape(-1, 2)))
+
+# Using Polynomial features
+X_ = PolynomialFeatures(degree=2, include_bias=True).fit_transform(X)
+model.fit(X_, y)
+print(model.intercept_, model.coef_, model.score(X_, y))
