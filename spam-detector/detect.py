9cf9930bd8c16688e65574af2d32f16d01978ee0
chetan22 <ck.chetan20@gmail.com>
2019-05-06 20:52:56 +0530

    Add size-based learning curve

--- spam-detector/detect.py
+++ spam-detector/detect.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import learning_curve
 import matplotlib.pyplot as plt
 
+plt.style.use('seaborn')
 
 df = pd.read_csv('./spam.csv', encoding='latin-1')
 X = df['v2'].values
@@ -38,3 +39,23 @@ model_ = RidgeClassifierCV(alphas=alpha)
 model_.fit(X_cv, y_cv)
 print(model_.score(X_test, y_test))
 print(model_.alpha_)
+
+# Plotting Learning curves based on length of training set
+# setting training-size options
+train_sizes = [1, 100, 200, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000]
+train_sizes, train_scores, validation_scores = learning_curve(
+                                    estimator=RidgeClassifier(),
+                                    X=vectorizer.fit_transform(X),
+                                    y=y, train_sizes=train_sizes,
+                                    cv=5, scoring='accuracy')
+print('training scores:\n', train_scores)
+print('validation_scores:\n', validation_scores)
+train_scores_mean = train_scores.mean(axis=1)
+validation_scores_mean = validation_scores.mean(axis=1)
+# plotting curve
+plt.plot(train_sizes, train_scores_mean, label='Training score')
+plt.plot(train_sizes, validation_scores_mean, label='Validation score')
+plt.ylabel('SCORE')
+plt.xlabel('Traing Size')
+plt.legend()
+plt.show()
