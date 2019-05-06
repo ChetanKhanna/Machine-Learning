24c7ac7fe5cb5c9f0a23e3db6a1a51921a829ef4
chetan22 <ck.chetan20@gmail.com>
2019-05-07 01:35:58 +0530

    Add feature-based validation curve

--- regularization/reg.py
+++ regularization/reg.py
@@ -8,14 +8,22 @@ from sklearn.linear_model import Ridge, Lasso
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler
 
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import learning_curve
+from sklearn.model_selection import validation_curve
+from sklearn.model_selection import GridSearchCV
 
 import matplotlib.pyplot as plt
 
 plt.style.use('seaborn')
+
+
+# defining PolynomialRegression
+def PolynomialRegression(degree=2, **kwargs):
+    return make_pipeline(PolynomialFeatures(degree),
+                         LinearRegression(**kwargs))
 
 
 # defining fuction for plotting learning curves
@@ -26,8 +34,6 @@ def learning_curves_(estimator, X, y, train_sizes, cv, scoring):
     # finding mean of scores
     train_scores_mean = -train_scores.mean(axis=1)
     validation_scores_mean = -validation_scores.mean(axis=1)
-    print(train_scores_mean, validation_scores_mean)
-    print(train_sizes)
     plt.plot(train_sizes, train_scores_mean, label='TrainingError')
     plt.plot(train_sizes, validation_scores_mean, label='ValidationError')
     plt.legend()
@@ -109,18 +115,6 @@ print('Testing accuracy:', lasso_pipeline.score(X_test, y_test))
 
 # plotting learning curve
 train_sizes = [100, 120, 150, 200, 250]
-# train_sizes, train_scores, validation_scores = learning_curve(
-#                             estimator=Lasso(alpha=0.3, fit_intercept=True),
-#                             X=X, y=y, train_sizes=train_sizes,
-#                             cv=5, scoring='neg_mean_squared_error')
-
-# train_scores_mean = -train_scores.mean(axis=1)
-# validation_scores_mean = -validation_scores.mean(axis=1)
-# plt.plot(train_sizes, train_scores_mean, label='TrainingError')
-# plt.plot(train_sizes, validation_scores_mean, label='ValidationError')
-# plt.legend()
-# plt.show()
-# plotting
 estimators = [Lasso(alpha=0.3, fit_intercept=True),
               LinearRegression(),
               Ridge(alpha=5, fit_intercept=True),
@@ -131,4 +125,42 @@ for model in estimators:
     learning_curves_(model, X, y, train_sizes, 5, 'neg_mean_squared_error')
     count += 1
 plt.tight_layout()
+plt.show()
+
+# plotting validation curve for deciding alpha for Ridge
+alpha_vals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+train_scores, validation_scores = validation_curve(Ridge(), X,
+                                                   y, 'alpha', alpha_vals,
+                                                   cv=5)
+train_scores_mean = train_scores.mean(axis=1)
+validation_scores_mean = validation_scores.mean(axis=1)
+plt.plot(alpha_vals, train_scores_mean, label='TrainingScore')
+plt.plot(alpha_vals, validation_scores_mean, label='ValidationScore')
+plt.legend()
+plt.show()
+
+# plotting validation curve for deciding alpha for Lasso
+alpha_vals = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+train_scores, validation_scores = validation_curve(Lasso(), X,
+                                                   y, 'alpha', alpha_vals,
+                                                   cv=5)
+train_scores_mean = train_scores.mean(axis=1)
+validation_scores_mean = validation_scores.mean(axis=1)
+plt.plot(alpha_vals, train_scores_mean, label='TrainingScore')
+plt.plot(alpha_vals, validation_scores_mean, label='ValidationScore')
+plt.legend()
+plt.show()
+
+# Deciding degree for Polynomial features
+degree_vals = [1, 2, 3, 4, 5, 6, 7]
+train_scores, validation_scores = validation_curve(PolynomialRegression(), X,
+                                                   y,
+                                                   'polynomialfeatures__degree',
+                                                   degree_vals, cv=5
+                                                   )
+train_scores_mean = train_scores.mean(axis=1)
+validation_scores_mean = validation_scores.mean(axis=1)
+plt.plot(degree_vals, train_scores_mean, label='TrainingScore')
+plt.plot(degree_vals, validation_scores_mean, label='ValidationScore')
+plt.legend()
 plt.show()
